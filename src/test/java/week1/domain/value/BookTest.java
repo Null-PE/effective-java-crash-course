303b8fe9e97203a5ef6cb624b8e6a3f33332dd75
kobain-jp <55679081+kobain-jp@users.noreply.github.com>
2021-09-16 14:33:21 +0900

    Update BookTest.java

--- src/test/java/week1/domain/value/BookTest.java
+++ src/test/java/week1/domain/value/BookTest.java
@@ -14,7 +14,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.hamcrest.collection.IsIterableContainingInOrder;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
