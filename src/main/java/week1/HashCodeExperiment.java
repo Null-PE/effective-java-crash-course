c813c050071db16387ba2248f3468142ac7aab00
yoshifumi <yoshifumi@kobain.local>
2021-09-16 20:46:09 +0900

    docs:week1

--- src/main/java/week1/HashCodeExperiment.java
+++ src/main/java/week1/HashCodeExperiment.java
@@ -12,7 +12,7 @@ public class HashCodeExperiment {
 		//特定の条件に特定のユーザが合致するかの結果を保持するMap
 		Map<CacheKey, Boolean> cacheResults = new HashMap<CacheKey, Boolean>();
 
-		// 何かのロジックを実行し、特定の条件で特定のuserの判定がtrueだった
+		// 何かのロジックを実行し、条件1にuser1が合致したので結果にtrueを詰める
 		long condId = 1;
 		long userId = 1;
 		CacheKey cacheKey = new CacheKey(condId, userId);
@@ -20,6 +20,7 @@ public class HashCodeExperiment {
 
 		// 別のロジックで対象のユーザの判定がどうだったかを確認する。
 		Boolean result = cacheResults.getOrDefault(new CacheKey(1, 1), Boolean.valueOf(false));
+		// TODO4 trueが出力されるように修正する。
 		System.out.println(result);
 	}
 
