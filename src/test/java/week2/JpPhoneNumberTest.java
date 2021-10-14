36e43d24b86a0b27c1141d7505ac878db5eadf4a
NULLPE <1989leosun@gmail.com>
2021-10-14 13:55:15 +0900

    week2: test compareTo

--- src/test/java/week2/JpPhoneNumberTest.java
+++ src/test/java/week2/JpPhoneNumberTest.java
@@ -1,13 +1,16 @@
 package week2;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class JpPhoneNumberTest {
@@ -32,7 +35,7 @@ public class JpPhoneNumberTest {
             "012-12-1234, 012, 12, 1234",
             "0123-432-8888, 0123, 432, 8888",
     })
-    void parse(String input, String areaCode, String cityCode, String sub) throws Exception{
+    void parse(String input, String areaCode, String cityCode, String sub) throws Exception {
         JpPhoneNumber target = JpPhoneNumber.parse(input);
         assertThat(target.getAreaCodeWithZeroPrefix(), is(areaCode));
         assertThat(target.getCityCode(), is(cityCode));
@@ -46,9 +49,9 @@ public class JpPhoneNumberTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"1123,1234,1234","0,1234,1234","01234,1234,1234",
-            "0123,12345,1234", "0123,1234,1","0123,1234,12345",
-            "012a,1234,1234","0123,123a,1234","0123,1234,123a",})
+    @CsvSource(value = {"1123,1234,1234", "0,1234,1234", "01234,1234,1234",
+            "0123,12345,1234", "0123,1234,1", "0123,1234,12345",
+            "012a,1234,1234", "0123,123a,1234", "0123,1234,123a",})
     void invalidArguments(String areaCode, String cityCode, String sub) throws Exception {
         assertThrows(IllegalArgumentException.class, () -> JpPhoneNumber.of(areaCode, cityCode, sub));
     }
@@ -65,5 +68,40 @@ public class JpPhoneNumberTest {
         assertThat(target.getCityCode(), is(cityCode));
         assertThat(target.getSubscriberNumber(), is(sub));
     }
+
+    @Nested
+    class CompareToTest {
+        @DisplayName("比較対象はNULL")
+        @Test
+        void compareToNull() throws Exception {
+            JpPhoneNumber j1 = JpPhoneNumber.of("0123", "1234", "1234");
+            assertThrows(NullPointerException.class, () -> j1.compareTo(null));
+        }
+
+        @DisplayName("同じ番号")
+        @Test
+        void equal() throws Exception {
+            JpPhoneNumber j1 = JpPhoneNumber.of("010", "100", "1000");
+            JpPhoneNumber j2 = JpPhoneNumber.of("010", "100", "1000");
+            assertThat(j1.compareTo(j2), is(0));
+        }
+
+        @ParameterizedTest(name = "比較対象より大きい 010-100-1000 : {0}-{1}-{2} ")
+        @CsvSource(value = {"001,100,1001", "010,100,0200"})
+        void bigger(String areaCode, String cityCode, String sub) throws Exception {
+            JpPhoneNumber j1 = JpPhoneNumber.of("010", "100", "1000");
+            JpPhoneNumber j2 = JpPhoneNumber.of(areaCode, cityCode, sub);
+            assertThat(j1.compareTo(j2), greaterThan(0));
+        }
+
+        @ParameterizedTest(name = "比較対象より小さい 010-100-1000 : {0}-{1}-{2} ")
+        @CsvSource(value = {"02,100,1000", "010,2,1000", "010,100,1002"})
+        void smaller(String areaCode, String cityCode, String sub) throws Exception {
+            JpPhoneNumber j1 = JpPhoneNumber.of("010", "100", "1000");
+            JpPhoneNumber j2 = JpPhoneNumber.of(areaCode, cityCode, sub);
+            assertThat(j1.compareTo(j2), lessThan(0));
+        }
+    }
+
 
 }
