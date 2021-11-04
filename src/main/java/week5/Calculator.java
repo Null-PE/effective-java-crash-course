3c6ead2da28b133571c32583dc1c102cebf7bba0
yoshifumi <yoshifumi@kobain.local>
2021-11-04 17:11:02 +0900

    feat:week5

--- src/main/java/week5/Calculator.java
+++ src/main/java/week5/Calculator.java
@@ -5,23 +5,22 @@ public class Calculator {
 	public static void main(String[] args) {
 
 		String command = args[0];
-		int x = Integer.valueOf(args[1]);
-		int y = Integer.valueOf(args[2]);
+		double x = Double.parseDouble(args[1]);
+		double y = Double.parseDouble(args[2]);
 
-		// refactor by using enum
+		// TODO refactor by using enum
 		// Command commandEnum = Command.valueOf(command);
-		// System.out.printf("%f %s %f = %f%n", x, commandEnum.getSynbol, y, "=",
+		// System.out.printf("%.1f %s %.1f = %f%n", x, commandEnum.getSynbol, y, "=",
 		// commandEnum.execute(x,y));
-		switch (command) {
-		case "PLUS":
-			System.out.printf("%f %s %f = %f%n", x, "+", y, "=", x + y);
-		case "MINUS":
-			System.out.printf("%f %s %f = %f%n", x, "-", y, "=", x - y);
-		case "TIMES":
-			System.out.printf("%f %s %f = %f%n", x, "*", y, "=", x * y);
-		case "DIVIDE":
-			System.out.printf("%f %s %f = %f%n", x, "/", y, "=", x / y);
-		default:
+		if ("PLUS".equalsIgnoreCase(command)) {
+			System.out.printf("%.1f %s %.1f = %.1f%n", x, "+", y, x + y);
+		} else if ("MINUS".equalsIgnoreCase(command)) {
+			System.out.printf("%.1f %s %.1f = %.1f%n", x, "-", y, x - y);
+		} else if ("MINUS".equalsIgnoreCase(command)) {
+			System.out.printf("%.1f %s %.1f = %.1f%n", x, "*", y, x * y);
+		} else if ("MINUS".equalsIgnoreCase(command)) {
+			System.out.printf("%.1f %s %.1f = %.1f%n", x, "/", y, x / y);
+		} else {
 			throw new IllegalArgumentException("Unsupported Commnad");
 		}
 
