572f6a691ecf30fe52a2fe4cc74da49547752c58
kobain-jp <55679081+kobain-jp@users.noreply.github.com>
2021-11-12 00:40:59 +0900

    Update Calculator.java

--- src/main/java/week5/q1/Calculator.java
+++ src/main/java/week5/q1/Calculator.java
@@ -4,10 +4,6 @@ public class Calculator {
 
 	public static int calculate(String commandStr, int x, int y) {
 
-		//TODO 1
-		//Command command = Command.valueOf(commandStr.toUpperCase());
-		//return command.execute(x,y);
-
 		if ("PLUS".equalsIgnoreCase(commandStr)) {
 			return x + y;
 		} else if ("MINUS".equalsIgnoreCase(commandStr)) {
@@ -18,5 +14,11 @@ public class Calculator {
 			return x / y;
 		}
 		throw new IllegalArgumentException("Unsupported Commnad");
+		
+		//TODO 1　implemnt the following and replace with the above
+		//Command command = Command.valueOf(commandStr.toUpperCase());
+		//return command.execute(x,y);
+		
+		
 	}
 }
