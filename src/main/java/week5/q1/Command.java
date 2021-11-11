f59157bcf2cd729a8dbbad3a30d761356752f1d4
yoshifumi <yoshifumi@kobain.local>
2021-11-12 00:34:36 +0900

    feat:command

--- src/main/java/week5/q1/Command.java
+++ src/main/java/week5/q1/Command.java
@@ -2,10 +2,12 @@ package week5.q1;
 
 public enum Command {
 
-	PLUS, MINUS, TIMES, DIVIDE
+	PLUS, MINUS, TIMES, DIVIDE;
+	
+	
 	
 	//TODO 1 implement
-	//public abstract double execute(x,y);
+	//public abstract double execute(int x,int y);
 	
 
 }
