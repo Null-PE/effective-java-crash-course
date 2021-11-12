bc139fec37cfd5c2ec0f7a54be97993616fca533
yoshifumi <yoshifumi@kobain.local>
2021-11-12 17:09:24 +0900

    feat:week5

--- src/main/java/week5/q1/advanced/Calculator.java
+++ src/main/java/week5/q1/advanced/Calculator.java
@@ -1,28 +1,32 @@
 package week5.q1.advanced;
 
+import com.google.common.annotations.VisibleForTesting;
+
 public class Calculator {
 
 	public static int calculateHardForTesting(String commandStr, int x, int y) {
-		//Enumで受けてしまうパターン
+		// Enumで受けてしまうパターン
 		BasicCommand command = BasicCommand.valueOf(commandStr.toUpperCase());
 		return doCalculate(command, x, y);
 	}
-	
+
 	// このメソッドの単体テストをするためにBasicCommandが必要で、モックできない
 	// テスト容易性が低い
-	private static int doCalculate(BasicCommand command, int x, int y) {
+	@VisibleForTesting
+	static int doCalculate(BasicCommand command, int x, int y) {
 		return command.execute(x, y);
 	}
-	
+
 	public static int calculateEasyForTesting(String commandStr, int x, int y) {
-		//Enumの実装しているInterfaceで宣言するパターン
+		// Enumの実装しているInterfaceで宣言するパターン
 		Command command = BasicCommand.valueOf(commandStr.toUpperCase());
 		return doCalculate(command, x, y);
 	}
 
 	// このメソッドの単体テストをするためにCommandのモックが利用できる
 	// テスト容易性が高い
-	private static int doCalculate(Command command, int x, int y) {
+	@VisibleForTesting
+	static int doCalculate(Command command, int x, int y) {
 		return command.execute(x, y);
 	}
 }
