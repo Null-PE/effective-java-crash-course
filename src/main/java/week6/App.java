fee224510c8065e9aebebe38255a2be1f5753fee
yoshifumi <yoshifumi@kobain.local>
2021-11-18 13:42:26 +0900

    add week6

--- src/main/java/week6/App.java
+++ src/main/java/week6/App.java
@@ -3,13 +3,22 @@ package week6;
 public class App {
 
 	public static void main(String[] args) {
+		
+		// 課題
+		// - 可読性
+		// - 安全性:requiredを強制したい
+		// - 設計性:不変オブジェクトにしたい
+		
 		// テレスコーピングパターン
-		// 書く時も読むときも何番目がどの引数かがわかりにくい
+		// ×:可読性:書く時も読むときも何番目がどの引数かがわかりにくい
+		// ○:安全性:requiredを強制できる
+		// ○:安全性:不変オブジェクトにできる
 		NutritionFacts_TeleScoping cocaCola = new NutritionFacts_TeleScoping(240, 8, 100, 35, 27, 0);
 
-		// JavaBeanパターン
-		// requiredのセットを強制できない
-		// 不変オブジェクトにできない
+		// JavaBeanパターン1
+		// ○:可読性:何をセットしているかがわかりやすい
+		// ×:安全性:requiredを強制できない
+		// ×:安全性:不変オブジェクトにできない
 		NutritionFacts_JavaBean pepsi = new NutritionFacts_JavaBean();
 		pepsi.setServingSize(240);
 		pepsi.setServings(240);
@@ -17,7 +26,10 @@ public class App {
 		pepsi.setSodium(100);
 		pepsi.setCarbohydrate(100);
 
-		// 不変オブジェクトにできない
+		// JavaBeanパターンの改善
+		// ○:可読性:何をセットしているかがわかりやすい
+		// ○:安全性:requiredを強制できる
+		// ×:安全性:不変オブジェクトにできない
 		NutritionFacts_JavaBean_Required redbull = new NutritionFacts_JavaBean_Required(240, 240);
 		redbull.setCalories(100);
 		redbull.setSodium(100);
--- src/main/java/week6/NutritionFacts_TeleScoping.java
+++ src/main/java/week6/NutritionFacts_TeleScoping.java
@@ -2,22 +2,21 @@ package week6;
 
 public class NutritionFacts_TeleScoping {
 
-	private final int servingSize; // mL required
-	private final int servings; // required
-	private final int calories; // optional
-	private final int fat; // optional
-	private final int sodium; // optional
-	private final int carbohydrate; // optional
+	private final int servingSize; // 1個分の分量 mL required
+	private final int servings; // 1箱あたり何個 required
+	private final int calories; // 1個分のカロリー optional
+	private final int fat; // 1個分の脂肪 optional
+	private final int sodium; // 1個分の塩分 optional
+	private final int carbohydrate; // 1個分の糖質 optional
 
 	// only required
 	public NutritionFacts_TeleScoping(int servingSize, int servings) {
-
 		this(servingSize, servings, 0, 0, 0, 0);
-
 	}
 
 	// required and optional
-	public NutritionFacts_TeleScoping(int servingSize, int servings, int calories, int fat, int sodium, int carbohydrate) {
+	public NutritionFacts_TeleScoping(int servingSize, int servings, int calories, int fat, int sodium,
+			int carbohydrate) {
 
 		this.servingSize = servingSize;
 		this.servings = servings;
