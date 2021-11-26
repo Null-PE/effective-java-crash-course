7271d2c3cb9de8acbe398d6d68b2aa387e2c0903
yoshifumi <yoshifumi@kobain.local>
2021-11-26 16:16:44 +0900

    test:week7

--- src/test/java/week7/InstrumentedHashSet_01ExtendedPatternTest.java
+++ src/test/java/week7/InstrumentedHashSet_01ExtendedPatternTest.java
@@ -3,6 +3,8 @@ package week7;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 class InstrumentedHashSet_01ExtendedPatternTest {
@@ -20,9 +22,7 @@ class InstrumentedHashSet_01ExtendedPatternTest {
 	@Test
 	void testAddAll() {
 		InstrumentedHashSet_01ExtendedPattern<String> set = new InstrumentedHashSet_01ExtendedPattern<String>();
-		set.add("A");
-		set.add("A");
-		set.add("A");
+		set.addAll(Arrays.asList("A","B","C"));
 		assertThat(set.getAddCount(), is(3));
 	}
 
--- src/test/java/week7/InstrumentedHashSet_02CompositePatternTest.java
+++ src/test/java/week7/InstrumentedHashSet_02CompositePatternTest.java
@@ -3,6 +3,8 @@ package week7;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 class InstrumentedHashSet_02CompositePatternTest {
@@ -20,9 +22,7 @@ class InstrumentedHashSet_02CompositePatternTest {
 	@Test
 	void testAddAll() {
 		InstrumentedHashSet_02CompositePattern<String> set = new InstrumentedHashSet_02CompositePattern<String>();
-		set.add("A");
-		set.add("A");
-		set.add("A");
+		set.addAll(Arrays.asList("A","B","C"));
 		assertThat(set.getAddCount(), is(3));
 	}
 
