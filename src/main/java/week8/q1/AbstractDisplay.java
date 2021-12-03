4c02692f46c257459b40c7a896f1be6ddc238163
yoshifumi <yoshifumi@kobain.local>
2021-12-03 15:28:50 +0900

    add week8

--- src/main/java/week8/q1/AbstractDisplay.java
+++ src/main/java/week8/q1/AbstractDisplay.java
@@ -3,7 +3,9 @@ package week8.q1;
 // TODO 1 implement this class and pass AbstractDisplayTest
 // 参考　Java言語で学ぶデザインパターン https://www.hyuki.com/dp/
 // テンプレートメソッドのメリット:saソース重複排除＋新規実装しやすい＋サブクラスを同一視できる（使う側はAbstractDisplayとして利用できる）
-// JDKの参考クラスはjava.io.InputStream#やjava.io.OutputStream
+// JDKの参考クラスは
+// java.io.InputStream#read(byte b[], int off, int len)がテンプレートメソッド
+// java.io.OutputStream#write(byte b[], int off, int len)がテンプレートメソッド
 public abstract class AbstractDisplay {
 
 	public final String createDisplayString() {
